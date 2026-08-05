5117655be880afad3cbb6c3cb0cf84bb8fa8c34a
Sviat6239 <sviatoslav6239@gmail.com>
2026-08-05 07:42:04 +0200

    Refactor core.asm to correct register usage, improve command handling, and update string outputs

--- core.asm
+++ core.asm
@@ -3,16 +3,15 @@
 .p2align 3
 
 main:
-    mov     x9, x1
+    mov     x19, x1
 
-    ldr     x22, [x9, #0x40]
-
-    ldr     x20, [x9, #0x08]
+    ldr     x22, [x19, #0x40]
+    ldr     x20, [x19, #0x30]
 
     mov     x0, x22
     adrp    x1, HelloStr
     add     x1, x1, :lo12:HelloStr
-    blr     PrintString
+    bl      PrintString
 
 PromptLoop:
     mov     x0, x22
@@ -22,7 +21,6 @@ PromptLoop:
 
     adrp    x23, InputBuffer
     add     x23, x23, :lo12:InputBuffer
-    mov     w24, #0
 
 ReadCharLoop:
     ldr     x21, [x20, #0x08]
@@ -41,17 +39,19 @@ ReadCharLoop:
 
     cbz     w25, ReadCharLoop
 
+    mov     w2, #0
     strh    w25, [x1]
-    strh    w0, [x1, #2]
+    strh    w2,  [x1, #2]
     mov     x0, x22
-    blr     x22
+    bl      PrintString
 
     strh    w25, [x23]
     add     x23, x23, #2
     b       ReadCharLoop
 
 ExecuteCommand:
-    strh    w0, [x23]
+    mov     w2, #0
+    strh    w2, [x23]
 
     mov     x0, x22
     adrp    x1, NewLineStr
@@ -67,16 +67,29 @@ ExecuteCommand:
     bl      StrCmp16
     cbz     w0, DoHelp
 
+    mov     x0, x22
+    adrp    x1, ExecutedStr
+    add     x1, x1, :lo12:ExecutedStr
+    bl      PrintString
+    b       PromptLoop
+
+DoHelp:
+    mov     x0, x22
+    adrp    x1, HelpMsgStr
+    add     x1, x1, :lo12:HelpMsgStr
+    bl      PrintString
+    b       PromptLoop
+
 PrintString:
     ldr     x3, [x0, #0x08]
-    br      x3
-
+    blr     x3
+    ret
 
 StrCmp16:
     ldrh    w2, [x0], #2
     ldrh    w3, [x1], #2
 
-    cmp     w, w3
+    cmp     w2, w3
     b.ne    StrCmp_Diff
 
     cbz     w2, StrCmp_Equal
@@ -108,13 +121,22 @@ NewLineStr:
     .hword 13, 10, 0
 
 ExecutedStr:
-    .hword '[', 'O', 'K', ']', 13, 10, 0
+    .hword 'U', 'n', 'k', 'n', 'o', 'w', 'n', ' '
+    .hword 'c', 'o', 'm', 'm', 'a', 'n', 'd', '.', 13, 10, 0
+
+HelpMsgStr:
+    .hword 'S', 'v', 'i', 'a', 't', 'O', 'S', ' '
+    .hword 'H', 'e', 'l', 'p', ':', 13, 10
+    .hword ' ', ' ', 'h', 'e', 'l', 'p', ' '
+    .hword '-', ' ', 's', 'h', 'o', 'w', ' '
+    .hword 't', 'h', 'i', 's', ' '
+    .hword 'm', 'e', 's', 's', 'a', 'g', 'e', 13, 10, 0
+
+CmdHelp:
+    .hword 'h', 'e', 'l', 'p', 0
 
 KeyBuffer:
     .hword 0, 0, 0
 
 InputBuffer:
     .space 256
-
-CmdHelp:
-    .hword 'h', 'e', 'l', 'p', 0
